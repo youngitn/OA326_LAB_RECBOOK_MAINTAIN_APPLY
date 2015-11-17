fa19100907e0467107b8098b9e3ea4d0b272574e
B0050 <B0050@yungshingroup.com>
2015-11-17 10:07:50 +0800

    新增 實驗室紀錄簿-原申請人 流程關卡規則.

---
+++ com/ysk/LAB_RECBOOK_MAINTAIN_APPLY/Rule/RecbookOriginalApplicantRule.java
@@ -0,0 +1,34 @@
+package com.ysk.LAB_RECBOOK_MAINTAIN_APPLY.Rule;
+//com/ysk/LAB_RECBOOK_MAINTAIN_APPLY/Rule/RecbookOriginalApplicantRule
+import java.util.Vector;
+
+import com.ysk.util.Log;
+import com.ysk.util.LogUtil;
+
+import SomeUtils._bRule;
+import SomeUtils.Bean.LabRecbookUsingApplyBean;
+import SomeUtils.DAO.LabRecbookUsingApplyDAO;
+/**
+ * 實驗室紀錄簿-原申請人關卡.
+ * @author YangTing
+ *
+ */
+public class RecbookOriginalApplicantRule extends _bRule{
+	Log log = LogUtil.getLog(this.getClass());
+	@Override
+	public Vector<String> getIDs(String arg0) throws Throwable {
+		
+		Vector<String> id = new Vector<String>();
+		// TODO Auto-generated method stub	
+		LabRecbookUsingApplyDAO l = new LabRecbookUsingApplyDAO();
+		
+		if (l != null){
+			LabRecbookUsingApplyBean lbean = l.getLabRecbookUsingApplyBean(getTalk(), getData("RECBOOK_NO"));
+			id.addElement(lbean.getREQ_EMPID());
+		}
+		id.addElement("admin");
+		
+		
+		return id;
+	}
+}
