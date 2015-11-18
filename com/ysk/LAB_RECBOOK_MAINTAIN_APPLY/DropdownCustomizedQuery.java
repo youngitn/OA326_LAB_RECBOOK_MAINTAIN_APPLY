c7734f19c0a90659728bbeb2e93def1c32f7723d
B0050 <B0050@yungshingroup.com>
2015-11-18 13:24:48 +0800

    查詢結果錯誤 要將兩之資料表作 where a.pno = b.pno 才是對的.

--- com/ysk/LAB_RECBOOK_MAINTAIN_APPLY/DropdownCustomizedQuery.java
+++ com/ysk/LAB_RECBOOK_MAINTAIN_APPLY/DropdownCustomizedQuery.java
@@ -18,13 +18,13 @@ public class DropdownCustomizedQuery extends _bQuery {
 		String otherConditionString = "";
 
 		// 研發"處" 所以取ParentNo
-		// 研發一 二課 的人 上一層是研發處 :13,此處用 getParentNo 涵蓋 一 ,二課 & 研發處的人
+		// 研發一 二課 的人 上一層是研發處 : LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.RD_DEPT_NO = 13,此處用 getParentNo 涵蓋 一 ,二課 & 研發處的人
 		if (user.getParentNo() == LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.RD_DEPT_NO
 				|| user.getDepNo() == LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.RD_DEPT_NO) {
 
 			otherConditionString += "and (a.RECBOOK_NO like 'YF%' OR a.RECBOOK_NO like 'YJ%')";
 
-		}// 品管"課": 18 或品管課底下的理化組等...
+		}// 品管"課": LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.QC_DEPT_NO = 18 或品管課底下的理化組等...
 		else if (user.getParentNo() == LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.QC_DEPT_NO
 				|| user.getDepNo() == LAB_RECBOOK_MAINTAIN_APPLY_FINAL_CONFIG.QC_DEPT_NO) {
 
@@ -40,7 +40,7 @@ public class DropdownCustomizedQuery extends _bQuery {
 		if(getRow()==-1){
 			return "";
 		} else {
-			return "and b.F_INP_STAT = '歸檔' " + otherConditionString;
+			return "and a.PNO = b.PNO and b.F_INP_STAT = '歸檔' " + otherConditionString;
 		}
 	}
 
